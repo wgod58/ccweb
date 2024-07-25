e19ea5225a3b9269f5b7b20222b46ef3c2799dd2
zedd <zeddpai@chapter-i.com>
2024-07-25 13:12:50 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,186 @@
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [isChecked, setIsChecked] = useState(false);
+  const [page, setPage] = useState(1);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const handleButtonClick = (event) => {
+    event.preventDefault();
+    if (!isChecked) alert("請同意上述條款和條件");
+    else setPage(2);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {page === 1 && (
+          <div>
+            <p>免責聲明與同意書</p>
+            <div className="neux-editor">
+              <p className="editor-element">
+                平台名稱
+                心動收容(下稱「本平台」)致力於為用戶提供高質量男女媒合服務。為確保服務的質量和用戶的知情權，特此提供以下免責聲明及同意書。請仔細閱讀並確認同意以下條款：
+              </p>
+              <p className="editor-element">&nbsp;</p>
+              <p className="editor-element">一、資料收集與使用</p>
+              <p className="editor-element">
+                1.個人資料的收集，本平台將您提供的個人資料，包括但不限於姓名、年齡、
+                性別、聯繫方式、興趣愛好等。
+              </p>
+              <p className="editor-element">
+                2.資料的使用，您的個人資料僅用於本平台內部的男女媒合服務，不會向第三方披露，除非經您明確同意或法律要求
+              </p>
+              <p className="editor-element">
+                3.您的資料將提供給其他來本平台消費的客戶，以便他們有機會抽取您的資料進行配對
+              </p>
+              <br />
+              <p className="editor-element">二、免責條款</p>
+
+              <p className="editor-element">
+                1.資料安全，本平台將盡力保護您的個人資料安全，但不對因不可抗力或第三方惡意行為導致的資料洩漏負責
+              </p>
+              <p className="editor-element">
+                2.配對結果，本平台不對男女配對的結果及後續發展負任何責任。配對成功與否以及後續關西的發展完全取決於雙方的互動和意願
+              </p>
+              <p className="editor-element">
+                3.客戶責任，您需對自己提供的資料的真實性和完整性負責，並同意對因提供虛假或不完整資料可能引發的後果承擔責任
+              </p>
+              <p className="editor-element">
+                <br />
+              </p>
+              <p className="editor-element">三、同意證明</p>
+              <p className="editor-element">
+                於下方點選勾選，並點選同意按鈕及代表您確認已閱讀並理解上述免責聲明，並同意本平台按上述條款使用您的個人資料
+              </p>
+              <p className="editor-element"></p>
+              <div>
+                <form>
+                  <div>
+                    <label>
+                      <input
+                        className="mr-2 leading-tight"
+                        type="checkbox"
+                        checked={isChecked}
+                        onChange={handleCheckboxChange}
+                        required
+                      />
+                      我同意上述條款和條件
+                    </label>
+                  </div>
+                  <div>
+                    <input
+                      className="bg-sky-500 hover:bg-sky-70 py-1 px-3 rounded-full"
+                      type="submit"
+                      onClick={handleButtonClick}
+                    />
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        )}
+        {page === 2 && (
+          <div className="py-5">
+            second page
+            <form className="w-full max-w-lg">
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3 mb-6 md:mb-0">
+                  <label
+                    className="block tracking-wide mb-2"
+                    for="grid-first-name"
+                  >
+                    姓名
+                  </label>
+                  <input
+                    className="text-black appearance-none block w-full bg-gray-200  border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                    id="grid-first-name"
+                    type="text"
+                  />
+                  <p className="text-red-500 text-xs italic">
+                    Please fill out this field.
+                  </p>
+                </div>
+                <div className="w-full  px-3">
+                  <label
+                    className="block tracking-wid mb-2"
+                    for="grid-last-name"
+                  >
+                    電話
+                  </label>
+                  <input
+                    className="text-black appearance-none block w-full bg-gray-200  border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-last-name"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                  <label
+                    className="block tracking-wide mb-2"
+                    for="grid-password"
+                  >
+                    Email
+                  </label>
+                  <input
+                    className="text-black appearance-none block w-full bg-gray-200  border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-password"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                  <label
+                    className="block tracking-wid mb-2"
+                    for="grid-password"
+                  >
+                    生日
+                  </label>
+                  <input
+                    className="text-black appearance-none block w-full bg-gray-200  border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-password"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                  <label
+                    className="block tracking-wid mb-2"
+                    for="grid-password"
+                  >
+                    身分證(選填)
+                  </label>
+                  <input
+                    className="text-black appearance-none block w-full bg-gray-200  border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-password"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div>
+                <button
+                  className="bg-sky-500 hover:bg-sky-70 py-1 px-3 rounded-full"
+                  type="submit"
+                  onClick={handleButtonClick}
+                >
+                  送出
+                </button>
+              </div>
+            </form>
+          </div>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
